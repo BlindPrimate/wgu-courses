b1ebbb3d8cd177767a12d06ceed2140fde502fdd
Eric Cleek <ecleek@lakenetwork.net>
2019-10-09 16:14:42 -0400

    base urls added

--- wgu_course/urls.py
+++ wgu_course/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from apps.course_selector import urls as course_selector_urls
 
 urlpatterns = [
+    path('', include(course_selector_urls)),
     path('admin/', admin.site.urls),
 ]
