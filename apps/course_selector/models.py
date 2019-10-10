c51f33575e9b4653bb2c3aa1e2d2dff2f9cdabaf
Eric Cleek <ecleek@lakenetwork.net>
2019-10-09 22:18:53 -0400

    models skeletonized

--- apps/course_selector/models.py
+++ apps/course_selector/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+from django.db import models 
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(blank=False, max_length=255)
+    description = models.CharField(blank=False, max_length=10000)
+    prereqs = models.ManyToManyField('self', blank=True, null=True)
+    comp_units = models.IntegerField(
+        default=3,
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(6)
+        ]
+    )
+
+    def __str__(self):
+        return self.name
+
+class Degree(models.Model):
+    name = models.CharField(blank=False, max_length=255)
+    description = models.CharField(blank=False, max_length=10000)
+    courses = models.ManyToManyField(Course)
+
+    def __str__(self):
+        return self.name
