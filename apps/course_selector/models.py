4c1818606a174791ee481c54c2c493b7beaffe03
Eric Cleek <ecleek@lakenetwork.net>
2019-10-10 00:12:16 -0400

    model updates

--- apps/course_selector/models.py
+++ apps/course_selector/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+
 class Course(models.Model):
+    course_number = models.CharField(blank=False, default=1, max_length=4)
     name = models.CharField(blank=False, max_length=255)
-    description = models.CharField(blank=False, max_length=10000)
-    prereqs = models.ManyToManyField('self', blank=True, null=True)
+    description = models.TextField(blank=False, max_length=10000)
+    prereqs = models.ManyToManyField('self', blank=True)
     comp_units = models.IntegerField(
         default=3,
         validators=[
@@ -13,14 +16,26 @@ class Course(models.Model):
             MaxValueValidator(6)
         ]
     )
+    class Meta:
+        ordering = ['course_number']
 
     def __str__(self):
-        return self.name
+        return self.course_number + " " + self.name
+        # return self.name
 
 class Degree(models.Model):
     name = models.CharField(blank=False, max_length=255)
     description = models.CharField(blank=False, max_length=10000)
     courses = models.ManyToManyField(Course)
+    degree_type = models.CharField(
+        max_length=4,
+        default="B.S.",
+        choices=[
+            ("B.S.", "Bachelors of Science"),
+            ("M.S.", "Masters of Science")
+        ]
+    )
 
     def __str__(self):
-        return self.name
+        return self.degree_type + " " + self.name
+        # return self.name
