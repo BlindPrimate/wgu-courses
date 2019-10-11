1480538f39570d5112499b6376a7b4cde2a79e89
Eric Cleek <ecleek@lakenetwork.net>
2019-10-10 21:29:28 -0400

    more model additions and database entries

--- apps/course_selector/admin.py
+++ apps/course_selector/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Course, Degree
+from .models import Course, Degree, Certification
 
 # Register your models here.
 
 admin.site.register(Course)
 admin.site.register(Degree)
+admin.site.register(Certification)
--- apps/course_selector/models.py
+++ apps/course_selector/models.py
@@ -3,13 +3,22 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
+class Certification(models.Model):
+    name = models.CharField(blank=False, max_length=255)
+    description = models.CharField(blank=True, max_length=10000)
+
+    def __str__(self):
+        return self.name
+
 
 class Course(models.Model):
-    course_number = models.CharField(blank=False, default=1, max_length=4)
     name = models.CharField(blank=False, max_length=255)
+    course_number = models.CharField(blank=False, default="C", max_length=4)
     description = models.TextField(blank=False, max_length=10000)
-    prereqs = models.ManyToManyField('self', blank=True)
+    prereqs = models.ManyToManyField('self', blank=True, verbose_name="Prerequisites")
+    certificate_earned = models.ManyToManyField(Certification, blank=True)
     comp_units = models.IntegerField(
+        'Competency Units',
         default=3,
         validators=[
             MinValueValidator(1),
@@ -17,16 +26,19 @@ class Course(models.Model):
         ]
     )
     class Meta:
-        ordering = ['course_number']
+        ordering = ['name']
 
     def __str__(self):
         return self.course_number + " " + self.name
-        # return self.name
+
+
 
 class Degree(models.Model):
     name = models.CharField(blank=False, max_length=255)
     description = models.CharField(blank=False, max_length=10000)
     courses = models.ManyToManyField(Course)
+    earned_certifications = models.ManyToManyField(Certification, blank=True)
+    link = models.URLField(blank=False, default="http://www.wgu.edu", max_length=200)
     degree_type = models.CharField(
         max_length=4,
         default="B.S.",
