7045e24a6ee1e0ddca876cd11640cc5e838b1b54
Eric Cleek <ecleek@lakenetwork.net>
2019-10-11 21:30:25 -0400

    javascript menus working

--- apps/course_selector/static/js/app.js
+++ apps/course_selector/static/js/app.js
@@ -1,2 +1,39 @@
+const course_card_links = Object.values(document.querySelectorAll('[data-card-index-link]'));
+const possible_click_targets = course_card_links.map((link) => {
+    return link.dataset.cardIndexLink;
+});
 
 
+const show_elements = (elements) => {
+    if(!NodeList.prototype.isPrototypeOf(elements)) {
+        elements.classList.remove('hidden');
+    } else {
+        elements.forEach((element) => {
+            element.classList.remove('hidden');
+        });
+    }
+}
+
+const hide_elements = (elements) => {
+    if(!NodeList.prototype.isPrototypeOf(elements)) {
+        elements.classList.add('hidden');
+    } else {
+        elements.forEach((element) => {
+            element.classList.add('hidden');
+        });
+    }
+}
+
+const switch_card = (card_id) => {
+    const all_cards = document.querySelectorAll('[data-card-index');
+    const target_card = document.querySelector(`[data-card-index='${card_id}']`)
+    hide_elements(all_cards);
+    show_elements(target_card);
+}
+
+window.addEventListener('mousedown', (x) => {
+    const click_target = x.target.dataset.cardIndexLink;
+    if (possible_click_targets.includes(click_target)) {
+        switch_card(click_target);
+    }
+})
--- apps/course_selector/views.py
+++ apps/course_selector/views.py
@@ -11,6 +11,10 @@ class CourseList(ListView):
     context_object_name = 'courses'
     template_name = 'course_selector/course_list.html'
     model = Course
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['knock'] = "knock"
+        return context
 
 class DegreesList(ListView):
     context_object_name = 'degrees'
